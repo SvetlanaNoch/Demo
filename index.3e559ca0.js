60027ca17564be4c088fe301c4ff1157eda45cd5
LuzhanskyiValerii <107570755+LuzhanskyiValerii@users.noreply.github.com>
2022-09-05 09:31:41 +0000

    Deploying to gh-pages from @ SvetlanaNoch/Team-project@373752e8155e0226bfd532436f185bb08dca4a60 🚀

---
+++ index.3e559ca0.js
@@ -0,0 +1,2 @@
+"use strict";$(".slick-carousel").slick({dots:!1,arrows:!1,infinite:!0,speed:500,slidesToShow:1,slidesToScroll:1,fade:!0,slide:"div",cssEase:"linear",autoplay:!0,autoplaySpeed:2e3});
+//# sourceMappingURL=index.3e559ca0.js.map
