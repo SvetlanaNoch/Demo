f51183a765dd23e1366742c980356d4f61f35ed3
LuzhanskyiValerii <107570755+LuzhanskyiValerii@users.noreply.github.com>
2022-09-05 14:33:05 +0000

    Deploying to gh-pages from @ SvetlanaNoch/Team-project@640dfef97e5065bcaec82a803004ad50a7fa8b78 🚀

---
+++ index.53e8afb5.js
@@ -0,0 +1,2 @@
+$("a").click((function(){$(slide_wrp).css({right:-$(slide_wrp).outerWidth()+"px"}),setTimeout((function(){$(slide_wrp).removeClass("active")}),50),$(overlay).css({opacity:"0",width:"0"})}));
+//# sourceMappingURL=index.53e8afb5.js.map
