ee83f5fb176ee0d74ced1cfcffbe26ece52d4911
User61 <svetsoff@gmail.com>
2022-09-06 16:44:45 +0300

    fix-bag

--- src/mobile-menu.js
+++ src/mobile-menu.js
@@ -5,6 +5,7 @@
       closeMenuBtn2: document.querySelector('[data-menu-close-2]'),
       closeMenuBtn3: document.querySelector('[data-menu-close-3]'),
       closeMenuBtn4: document.querySelector('[data-menu-close-4]'),
+      closeMenuBtn5: document.querySelector('[data-menu-close-5]'),
       menu: document.querySelector('[data-menu]'),
     };
     refs.openMenuBtn.addEventListener('click', toggleModal);
@@ -12,6 +13,7 @@
   refs.closeMenuBtn2.addEventListener('click', toggleModal);
   refs.closeMenuBtn3.addEventListener('click', toggleModal);
   refs.closeMenuBtn4.addEventListener('click', toggleModal);
+  refs.closeMenuBtn5.addEventListener('click', toggleModal);
     function toggleModal() {
       refs.menu.classList.toggle('is-open');
     }
