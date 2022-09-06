f2c30ca4efae62a5440241f2599be5f51e6c2350
SvetlanaNoch <108273059+SvetlanaNoch@users.noreply.github.com>
2022-09-06 13:25:12 +0000

    Deploying to gh-pages from @ SvetlanaNoch/Team-project@8bf82f95156fa1e294a3e4d89d6a1db5bbb21755 🚀

---
+++ index.7316e254.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-open-l]"),closeModalBtn:document.querySelector("[data-modal-close-l]"),modal:document.querySelector("[data-modal-l]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.7316e254.js.map
