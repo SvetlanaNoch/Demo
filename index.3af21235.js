b6534ae21580a2bdf4448c16d241d40e6c89ceb0
SvetlanaNoch <108273059+SvetlanaNoch@users.noreply.github.com>
2022-09-08 20:37:41 +0000

    Deploying to gh-pages from @ SvetlanaNoch/Team-project@c7d45bd40747ae1d00d6b386ae5948e01844f2b6 🚀

---
+++ index.3af21235.js
@@ -0,0 +1,2 @@
+$((function(){$(window).scroll((function(){0!=$(this).scrollTop()?$("#toTop").fadeIn():$("#toTop").fadeOut()})),$("#toTop").click((function(){$("body,html").animate({scrollTop:0},800)}))}));
+//# sourceMappingURL=index.3af21235.js.map
